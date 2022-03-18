548ef0672a3bd32c603fbbc567cd2fe892b22904
cicerotcv <elm.tiago@gmail.com>
2022-03-18 20:19:46 -0300

    fix readme

--- pycrypto/commandparser.py
+++ pycrypto/commandparser.py
@@ -12,12 +12,14 @@ def command_parser():
     # Input mode is "file"
     file_mode = input_mode.add_parser(name="file", help="Input a file to be encrypted")
     file_mode.add_argument(dest="filepath", help="Path of file to be encrypted")
-    file_mode.add_argument('--store-key', dest="store_key", help="Whether the program should store the generated key or not")
+    file_mode.add_argument('--store-key', dest="store_key", action='store_true', help="Whether the program should store the generated key or not")
+    file_mode.add_argument('-p', '--password', help="Password used to decrypt")
 
     # Input mode is "text"
     text_mode = input_mode.add_parser(name="text", help="Encrypt the following text") 
     text_mode.add_argument(dest="text", help="Text to be encrypted")
+    text_mode.add_argument('-p', '--password', help="Password used to decrypt")
 
-    encryption_group.add_argument('-p', '--password', help="Password used to decrypt")
+
 
     return parser.parse_args()
